64639657e9e64b5b59f23ba427544635ded710dd
wjs_5025 <wjs_5025@naver.com>
2023-03-27 20:58:50 +0900

    feat : 지역별여행지 마커 팝업 구현

--- WebContent/assets/js/kakaoMap.js
+++ WebContent/assets/js/kakaoMap.js
@@ -18,10 +18,9 @@ const displayMarker = () => {
         // 마커 이미지의 이미지 크기 입니다
         let imageSize = new kakao.maps.Size(24, 35);
 
-        imageSrc = `/assets/img/marker/marker${positions[i].contenttypeid}.png`;
+        imageSrc = `/assets/img/marker/marker${positions[i].contentTypeId}.png`;
         // 마커 이미지를 생성합니다
         let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-            console.log(positions[i])
         let markerInfo = {
             map: map, // 마커를 표시할 지도
             position: positions[i].latlng, // 마커를 표시할 위치
@@ -41,15 +40,15 @@ const displayMarker = () => {
                   <img class="card-img-top" src="${positions[i].firstImage}" alt="">
                   <div class="card-body">
                     <h5 class="card-title">${positions[i].title}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   </div>
                   <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${positions[i].addr1}</li>
+                    <li class="list-group-item">[주소]<br/>(${positions[i].zipCode}) ${positions[i].addr1}</li>
+                    <li class="list-group-item">[전화번호]<br/>${positions[i].tel}</li>
                   </ul>
                 </div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
             iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
-    // 인포윈도우를 생성합니다
+        // 인포윈도우를 생성합니다
         let infowindow = new kakao.maps.InfoWindow({
             content: iwContent,
             removable: iwRemoveable
@@ -69,4 +68,28 @@ const displayMarker = () => {
 
 function moveCenter(lat, lng) {
     map.setCenter(new kakao.maps.LatLng(lat, lng));
+}
+
+const contentIdToString = (contentId) => {
+    switch (contentId) {
+        case 12 :
+            return "관광지";
+        case 14:
+            return "문화시설";
+        case 15:
+            return "축제공연행사";
+        case 25:
+            return "여행코스";
+        case 28:
+            return "레포츠";
+        case 32:
+            return "숙박";
+        case 38:
+            return "쇼핑";
+        case 39:
+            return "음식점";
+        default :
+            return "기타"
+    }
+
 }
