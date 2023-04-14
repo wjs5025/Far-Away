ea45166e2366a3e5762090280e2ef7a527aa8c56
longbright0804 <longbright0804@gmail.com>
2023-04-14 14:09:01 +0900

    fix : pagenavigation

--- src/com/ssafy/attraction/info/controller/AttractionInfoController.java
+++ src/com/ssafy/attraction/info/controller/AttractionInfoController.java
@@ -48,6 +48,8 @@ public class AttractionInfoController extends HttpServlet {
             case "list":
                 list(request, response);
                 break;
+            case "pageNavigation":
+                pageNavigation(request, response);
             default:
                 break;
         }
@@ -81,17 +83,11 @@ public class AttractionInfoController extends HttpServlet {
                 map.put("contentTypeId", contentTypeId);
 
                 List<AttractionInfoDto> attractionInfoList = attractionInfoService.getAttractionInfoList(map);
-
-                PageNavigation pageNavigation = attractionInfoService.makePageNavigation(map);
-                request.setAttribute("navigation",pageNavigation);
                 // JSON test
                 StringBuilder resultJson = new StringBuilder();
                 try {
                     ObjectMapper objectMapper = new ObjectMapper();
                     resultJson.append(objectMapper.writeValueAsString(attractionInfoList));
-//                    resultJson.append(",");
-//                    resultJson.append(objectMapper.writeValueAsString(pageNavigation));
-//                    System.out.println(resultJson);
                     response.setContentType("application/x-json; charset=utf-8");
                     response.getWriter().println(resultJson);
                 } catch (Exception e) {
@@ -105,6 +101,37 @@ public class AttractionInfoController extends HttpServlet {
             return "/attraction/attraction-index.jsp" + queryString;
         } else {
             return "/user/user-login.jsp";
+        }
+    }
+
+    private void pageNavigation(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserDto userDto = (UserDto) session.getAttribute("user");
+        if (userDto != null) {
+            try {
+                Map<String, String> map = new HashMap<>();
+                map.put("pageNo", pageNo + "");
+                map.put("sidoCode", sidoCode);
+                map.put("gugunCode", gugunCode);
+                map.put("contentTypeId", contentTypeId);
+
+                PageNavigation pageNavigation = attractionInfoService.makePageNavigation(map);
+                request.setAttribute("navigation",pageNavigation);
+                // JSON test
+                StringBuilder resultJson = new StringBuilder();
+                try {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    resultJson.append(objectMapper.writeValueAsString(pageNavigation));
+                    response.setContentType("application/x-json; charset=utf-8");
+                    response.getWriter().println(resultJson);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                request.setAttribute("msg", "관광지 정보 조회에 실패했습니다.");
+            }
         }
     }
 }
